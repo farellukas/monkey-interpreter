14b0672c78b4979b08fbc9d8c87efd6844e4e0fd
Farel Lukas <farellukas@gmail.com>
2025-03-12 10:43:24 -0600

    Created REPL and finished keyword lexing

--- token/token.go
+++ token/token.go
@@ -48,8 +48,13 @@ const (
 )
 
 var keywords = map[string]TokenType{
-	"fn":  FUNCTION,
-	"let": LET,
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
 }
 
 func LookupIdent(ident string) TokenType {
